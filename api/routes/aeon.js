a9f633a358142e3a13abcc0879a66afe65d27ba8
John Nguyen <johnv.nguyen14@gmail.com>
2024-06-06 21:53:40 -0400

    Added CRUD to /routes/aeon.js.

--- api/routes/aeon.js
+++ api/routes/aeon.js
@@ -3,3 +3,16 @@ const aeonCtrl = require('../controllers/aeon.js');
 
 const router = Router();
 
+// POST Request
+router.post('/', aeonCtrl.createAeon);
+
+// GET Request
+router.get('/', aeonCtrl.getAeons);
+
+// PATCH Request
+router.patch('/:id', aeonCtrl.updateAeon);
+
+// DELETE Request
+router.delete('/:id', aeonCtrl.deleteAeon);
+
+module.exports = router;
